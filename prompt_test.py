6d5201e3c2986d1639b807633d199cae6b53052d
Maliny Po <po0007@student.monash.edu>
2025-07-09 16:27:32 +1000

    Update client responses to include image

--- prompt_test.py
+++ prompt_test.py
@@ -1,9 +1,24 @@
 import os
+import base64
 from openai import OpenAI
 from dotenv import load_dotenv
+from pathlib import Path
 
-with open('files/breilleR_example1.txt', 'r') as file:
+# Set up the prompt, brailleR and image
+
+with open('files/breilleR_example2.txt', 'r') as file:
     data = file.read().replace('\n', '')
+
+sysprompt = Path("prompt.txt").read_text()
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+    
+image_path = "files/example2.png"
+base64_image = encode_image(image_path)
+
+# Set up the Client -----------------------------
 
 load_dotenv("environment.env")
 client = OpenAI(
@@ -11,9 +26,21 @@ client = OpenAI(
 )
 
 response = client.responses.create(
-    model = "gpt-4.1",
-    instructions="You are a researcher that talk very concisely. Describe what you can learn from the plot",
-    input = data
+    model="gpt-4.1",
+    instructions="You are a researcher that talk very concisely",
+    # input = str(sysprompt) + str(data)
+    input=[
+        {
+            "role": "user",
+            "content": [
+                { "type": "input_text", "text": "what's in this image?" },
+                {
+                    "type": "input_image",
+                    "image_url": f"data:image/jpeg;base64,{base64_image}",
+                },
+            ],
+        }
+    ],
 )
 
 print(response.output_text)
